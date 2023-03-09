7f6616fe3e924e4875a306cd21cb63a6a5f84a68
facuaragon <facundo_aragon@hotmail.com>
2023-03-09 12:32:20 -0300

    commit

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -3,19 +3,39 @@ import Cards from './components/Cards/Cards.jsx'
 import Nav from "./components/Nav/Nav.jsx"
 import { useState } from 'react'
 
+// const onSearch = (character) => {
+  //   fetch(`https://rickandmortyapi.com/api/character/${character}`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+    //        if (data.name) {
+  //           setCharacters((oldChars) => [...oldChars, data]);
+  //        } else {
+    //           window.alert('No hay personajes con ese ID');
+    //        }
+    //     });
+    // }
+    
+    
 function App () {
   const [characters, setCharacters] = useState([]);
 
-  const onSearch = (character) => {
-    fetch(`https://rickandmortyapi.com/api/character/${character}`)
-      .then((response) => response.json())
-      .then((data) => {
-         if (data.name) {
-            setCharacters((oldChars) => [...oldChars, data]);
-         } else {
-            window.alert('No hay personajes con ese ID');
-         }
-      });
+  const onSearch = (id) => {
+    const URL_BASE = "https://be-a-rym.up.railway.app/api";
+    const KEY = "f34d1ed75dfb.cff0824d244e69fdada3";
+    fetch(`${URL_BASE}/character/${id}?key=${KEY}`)
+    .then(response=>response.json())
+    .then(data=>{
+      if(data.name && !characters.find(char=>char.id===data.id)){
+        setCharacters((oldChars)=>[...oldChars,data]);
+        //setCharacters([...characters, data]);
+      }
+      else if (characters.find(char=>char.id===data.id)){
+        alert("esta tarjeta ya se esta mostrando")
+      }
+      else{
+        alert("El id no existe o algo salío mal");
+      }
+    })
   }
 
   const onClose = (id) => {
