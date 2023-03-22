3babd1f55deda7cb78b59c7da98666ddc5acf078
facuaragon <facundo_aragon@hotmail.com>
2023-03-22 09:58:36 -0300

    correcciones segun code review 22-3-2023 con Jorge

--- rick_and_morty/back/src/routes/server.js
+++ rick_and_morty/back/src/routes/server.js
@@ -7,13 +7,20 @@ const PORT = 3001;
 http.createServer((req,res) =>{
     // console.log(`levantando server en puerto ${PORT}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
+    const {url} = req;
+    // console.log(url);
+
     if(req.url.includes('rickandmorty/character')){
-        const id = req.url.split('/').at(-1);
+        const id = url.split('/').at(-1);
+
         // const characterFilter = characters.filter(char => char.id === Number(id)) // me devuelve un array comn el objeto del personaje dentro
         const characterFilter = characters.find(char => char.id === Number(id)) //  me devuelve directamente el contenido del array, es decir, en este caso, un objeto
         console.log(characterFilter);
-        res
-        .writeHead(200, {"Content-type":"application/json"})
-        .end(JSON.stringify(characterFilter));
+
+        characterFilter 
+        ? res.writeHead(200, {"Content-type":"application/json"}).end(JSON.stringify(characterFilter))
+        : res.writeHead(400, {"Content-type":"application/json"}).end(JSON.stringify({error: "character not found"}))
+        
+        return;
     }
 }).listen(PORT, 'localhost')
